30be90091ddf6397d2b96b8f9ee2a4e95f66c756
shamekh65-boop <shamekh65@icloud.com>
2026-05-18 23:03:33 +0200

    sw.js aktualisieren

--- chauffeur/sw.js
+++ chauffeur/sw.js
@@ -11,3 +11,32 @@ self.addEventListener("fetch", event => {
     fetch(event.request).catch(() => caches.match(event.request))
   );
 });
+
+self.addEventListener("push", event => {
+
+  let data = {};
+
+  if (event.data) {
+    data = event.data.json();
+  }
+
+  self.registration.showNotification(
+    data.title || "Nieuwe rit",
+    {
+      body: data.body || "Er is een nieuwe rit beschikbaar",
+      icon: "/ymaanygo/chauffeur/icons/icon-192.png",
+      badge: "/ymaanygo/chauffeur/icons/icon-192.png"
+    }
+  );
+
+});
+
+self.addEventListener("notificationclick", event => {
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow("/ymaanygo/chauffeur/")
+  );
+
+});
